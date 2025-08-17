2d7ab7b735e4bf9a57c3295aaf836a308d62d895
Dan Sheffner <Dan@Sheffner.com>
2025-08-16 20:24:43 -0500

    adding compiled from source python3.12

--- bare_metal.sh
+++ bare_metal.sh
@@ -15,6 +15,7 @@ cd ansible_examples
 ansible-playbook -i hosts2 playbooks/update-apt-packages.yaml --connection=local
 ansible-playbook -i hosts2 playbooks/base-packages.yaml --connection=local
 ansible-playbook -i hosts2 playbooks/install-python311.yaml --connection=local
+ansible-playbook -i hosts2 playbooks/install-python312.yaml --connection=local
 ansible-playbook -i hosts2 playbooks/install-python313.yaml --connection=local
 ansible-playbook -i hosts2 playbooks/install-python314.yaml --connection=local
 ansible-playbook -i hosts2 playbooks/install-docker.yaml --connection=local
